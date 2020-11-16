0319fbab1215819a0e2109df7884e7fd558c6ef2
Mats <matslearning@outlook.com>
2020-11-16 23:53:15 +0100

    Seed for Documents, enum for course-module-activity

---
+++ LMS/Common/LmsResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS.Common
+{
+    public class LmsResult
+    {
+        public bool Succeeded { get; set; }
+
+        public string Message { get; set; }
+    }
+}
