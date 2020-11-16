ca2442be1f13c40c76f0b416855ef2de431c4ca1
ulfsv <ulf.e.sv@gmail.com>
2020-11-16 16:11:30 +0100

    Models -> Course.cs added validation row 16 datatype.text, row 18 display name on start date, row 19 datatype Date

--- LMS/Models/Course.cs
+++ LMS/Models/Course.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
+// Ulf added validation row 16 datatype.text, row 18 display name on start date, row 19 datatype Date
 
 namespace LMS.Models
 {
@@ -9,8 +12,11 @@ namespace LMS.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        
+        [DataType(DataType.Text)]
         public string Description { get; set; }
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date )]
         public DateTime StartDate { get; set; }
 
         // Navigation property
