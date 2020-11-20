f84d95a6b5dec28677c2325731eb829b7ff4dc66
Murtaza Rafi <murtaza_bayat@hotmail.com>
2020-11-20 10:40:14 +0100

    SQL Query get teachers

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿select users.Email, users.FirstName, users.LastName, roles.Name as Role
+from AspNetUsers as users
+join AspNetUserRoles as userroles
+on users.Id = userroles.UserId
+join AspNetRoles as roles
+on userroles.RoleId = roles.Id
+where roles.Name = 'TEACHER'
