4aeccc1384839f03e167554c4312f4a838e62439
mrbrunotte <mrbrunotte@gmail.com>
2020-11-20 10:06:09 +0100

    Add landingpage, style, controller path

--- LMS/Controllers/CoursesController.cs
+++ LMS/Controllers/CoursesController.cs
@@ -74,6 +74,29 @@ namespace LMS.Controllers
             return PartialView("CoursePartialDetails", course);
         }
 
+        //Student List
+        // GET: Student/List/5
+        public async Task<IActionResult> Students(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await db.Roles
+                .FirstOrDefaultAsync(s => s.Id == "6bba219d-3f1c-41a0-91d1-74306387792c");
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/PartialDetails/5
+        
+        // END Student List
+
         // GET: Courses/Create
         public IActionResult Create()
         {
--- LMS/Startup.cs
+++ LMS/Startup.cs
@@ -58,7 +58,7 @@ namespace LMS
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Courses}/{action=TeacherOverView}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
