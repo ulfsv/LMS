8b6c23106ced74aa21359eb440f09cdfe5947b5f
ulfsv <ulf.e.sv@gmail.com>
2020-11-30 16:59:09 +0100

    update

--- LMS/Models/ViewModels/StudentDetailsViewModel.cs
+++ LMS/Models/ViewModels/StudentDetailsViewModel.cs
@@ -7,10 +7,11 @@ namespace LMS.Models.ViewModels
 {
     public class StudentDetailsViewModel
     {
-        public Course Course { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-        public string StudentName { get; set; }
-
+        public string UserName { get; set; }
+        public string Email { get; set; }
     }
 }
 
